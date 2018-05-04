aa5933f9b42bbae2cee6e12751b50eec8d650ed1
JoeBrotzer <jomar.brotzer@gmail.com>
2018-05-04 14:00:56 +0800

    Modified models and admin

--- stream/apps.py
+++ stream/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class StreamConfig(AppConfig):
-    name = 'stream'
+class StreamingConfig(AppConfig):
+    name = 'streaming'
--- stream/models.py
+++ stream/models.py
@@ -9,29 +9,40 @@ class Streamer(models.Model):
         related_name='name'
     )
 
+    def __str__(self):
+        return self.username.get_full_name()
+
 
 # Not sure if this is right but ImageField requires pip install Pillow to use
 # If this is a wrong approach then just change it temporarily to CharField
 class Stream(models.Model):
     title = models.CharField(max_length=50)
-    thumbnail = models.ImageField(upload_to=None)
+    thumbnail = models.ImageField(upload_to='templates/images', blank=True)
     streamer = models.ForeignKey(Streamer, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
 
 
 # Not sure if this is right but ImageField requires pip install Pillow to use
 # If this is a wrong approach then just change it temporarily to CharField
 class Lobby(models.Model):
     lobbyname = models.CharField(max_length=50)
-    logo = models.ImageField(upload_to=None)
-    description = models.CharField(max_length=200)
+    # Set a default image
+    logo = models.ImageField(upload_to='templates/images', blank=True)
+    description = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.lobbyname
 
 
 # "Bridge" between streamers and lobby to prevent use of ManyToMany Field
 class Streamer_lobby(models.Model):
-    user = models.ForeignKey(
+    streamers = models.ForeignKey(
         Streamer,
         on_delete=models.PROTECT,
-        related_name='name'
+        related_name='name',
+        null=True
     )
     lobbies = models.ForeignKey(
         Lobby,
