e16ab745d46b817a2d120a0cc5f75780bf48264b
JoeBrotzer <jomar.brotzer@gmail.com>
2018-05-11 15:24:33 +0800

    modified stream selection

--- stream/static/stream/js/lobby.js
+++ stream/static/stream/js/lobby.js
@@ -1,9 +1,18 @@
 $(function(){
-    $(".thumbnail").click(function() {
-        var img = $(this).attr("src");
+    $(".stuff").click(function() {
+        var img = $(this).find(".thumbnail").attr("src");
+        var user = $(this).find("#streamer").text()
+        var title = $(this).find("#title").text()
+        
 
         $('#live img').fadeOut(400, function(){
             $('#live img').attr('src', img);
         }).fadeIn(400);
+
+
+        $('#live h5').fadeOut(400, function(){
+            $("#live h5").html(title + " - " + user);
+        }).fadeIn(400);
+
     });
 });
