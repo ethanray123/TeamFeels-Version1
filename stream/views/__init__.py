57247158c31848331614b6b5d453f2f5cea000d6
rendave <rendave.lecciones3@gmail.com>
2018-05-14 14:07:16 +0800

    added the create, update, delete lobby functionality

--- stream/views/__init__.py
+++ stream/views/__init__.py
@@ -1,5 +1,6 @@
 from .home import HomeView, SearchView
-from .lobby import LobbyView, CommentView
+from .lobby import LobbyView, CommentView, CreateLobbyView, \
+    UpdateLobbyView, DeleteLobbyView
 from .userform import UserFormView
 from .streamupdate import StreamFormView
 from .streamer import StreamerView
