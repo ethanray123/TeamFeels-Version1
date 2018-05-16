2a41cd3ece2620535b8a255906e08247f159f064
rendave <rendave.lecciones3@gmail.com>
2018-05-16 11:18:32 +0800

    removed comments and unused imports

--- stream/views/subscribe.py
+++ stream/views/subscribe.py
@@ -1,34 +1,11 @@
 import json
 
-from django.shortcuts import get_object_or_404, render
-from stream.models import Subscription, Streamer, Lobby
+from django.shortcuts import get_object_or_404
+from stream.models import Subscription, Streamer
 from django.views.generic import View
 from django.http import HttpResponse, Http404
 
 
-# def subscribe(request, streamer_id, lobby_id):
-#     subscriber = get_object_or_404(Streamer, user=request.user)
-#     subscribee = get_object_or_404(Streamer, pk=streamer_id)
-#     lobby = get_object_or_404(Lobby, pk=lobby_id)
-#     if(not subscribee.is_subscribed(subscriber) and
-#        subscriber.user.username != subscribee.user.username):
-#         Subscription.objects.create(
-#             subscriber=subscriber,
-#             publisher=subscribee)
-#         message = "you are now subscribed to " + subscribee.user.username + "!"
-#     else:
-#         if(subscribee.is_subscribed(subscriber)):
-#             message = "you have already subscribed to " \
-#                 + subscribee.user.username
-#         else:
-#             message = "you cannot subscribe to yourself"
-#     return render(
-#         request, 'stream/lobby.html',
-#         {
-#             'lobby': lobby,
-#             'message': message,
-#         }
-#     )
 class SubscribeView(View):
 
     def get(self, request, *args, **kwargs):
