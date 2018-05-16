296d0d4f076bc5a50c340703759c4aa6d1b131f5
Ethan Ray Flores Mosqueda <ethanray.mosqueda@gmail.com>
2018-05-16 10:51:36 +0800

    fixed creation of stream bug not filling up streamers field

--- stream/views/subscribe.py
+++ stream/views/subscribe.py
@@ -1,11 +1,12 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, reverse
 from stream.models import Subscription, Streamer, Lobby
+from django.http import HttpResponseRedirect
 
 
 def subscribe(request, streamer_id, lobby_id):
     subscriber = get_object_or_404(Streamer, user=request.user)
     subscribee = get_object_or_404(Streamer, pk=streamer_id)
-    lobby = get_object_or_404(Lobby, pk=lobby_id)
+    # lobby = get_object_or_404(Lobby, pk=lobby_id)
     if(not subscribee.is_subscribed(subscriber) and
        subscriber.user.username != subscribee.user.username):
         Subscription.objects.create(
@@ -18,10 +19,13 @@ def subscribe(request, streamer_id, lobby_id):
                 + subscribee.user.username
         else:
             message = "you cannot subscribe to yourself"
-    return render(
-        request, 'stream/lobby.html',
-        {
-            'lobby': lobby,
-            'message': message,
-        }
-    )
+
+    return HttpResponseRedirect(
+        reverse('stream:lobby-detail', args=[lobby_id,message]))
+    # return render(
+    #     request, 'stream/lobby.html',
+    #     {
+    #         'lobby': lobby,
+    #         'message': message,
+    #     }
+    # )
